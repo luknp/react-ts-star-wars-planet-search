85530d5342af69eb59639c2d671f1f99b5dc6684
luknp <luk.npl@gmail.com>
2021-11-28 17:13:14 +0100

    feat: fetch logic into hook

--- src/pages/Planets/Planets.tsx
+++ src/pages/Planets/Planets.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import SearchInput from 'components/SearchInput';
+import useFetchPlanets, { FetchStatus } from './hooks/useFetchPlanets';
 import { Planet } from 'types';
 import { getAllData } from 'utils';
 import './style.scss';
@@ -7,8 +8,8 @@ import './style.scss';
 const API_PLANETS_URL = 'https://swapi.dev/api/planets';
 
 export default function Planets() {
-  const [planets, setPlanets] = useState<Planet[]>([]);
   const [searchPhrase, setSearchPhrase] = useState('');
+  const { status, data, error } = useFetchPlanets(API_PLANETS_URL);
 
   const searchPlanets = (planets: Planet[], searchPhrase: string) => {
     return planets.filter((item: Planet) => {
@@ -16,17 +17,12 @@ export default function Planets() {
     });
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getAllData(API_PLANETS_URL);
-        setPlanets(data);
-      } catch (error: any) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, []);
+  const checkStatus = (expectedStatus: FetchStatus): boolean => {
+    if (status === expectedStatus) {
+      return true;
+    }
+    return false;
+  };
 
   return (
     <div className='planets'>
@@ -35,13 +31,17 @@ export default function Planets() {
       </header>
       <SearchInput searchPhrase={searchPhrase} setSearchPhrase={setSearchPhrase} placeholder='Search for a planet' />
       <div className='planets__content flex-center'>
-        <ul className='planets__content-cards'>
-          {searchPlanets(planets, searchPhrase).map((planet: Planet) => (
-            <li key={planet.name} className=''>
-              <p>{planet.name}</p>
-            </li>
-          ))}
-        </ul>
+        {checkStatus(FetchStatus.Loading) && <p>Loading..</p>}
+        {checkStatus(FetchStatus.Error) && <p>{error?.message}</p>}
+        {checkStatus(FetchStatus.Fetched) && (
+          <ul className='planets__content-cards'>
+            {searchPlanets(data, searchPhrase).map((planet: Planet) => (
+              <li key={planet.name} className=''>
+                <p>{planet.name}</p>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
