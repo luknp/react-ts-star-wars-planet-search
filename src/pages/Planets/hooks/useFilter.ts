0df4eab7e6528a4359232f8f3aecd227c6621cca
luknp <luk.npl@gmail.com>
2021-11-28 17:21:13 +0100

    refac(filter): filteredByStartWith at the beginning of array

--- src/pages/Planets/hooks/useFilter.ts
+++ src/pages/Planets/hooks/useFilter.ts
@@ -5,11 +5,19 @@ function useFilter(searchPhrase: string, data: Planet[]) {
   const [filteredData, setFilteredData] = useState(data);
 
   useEffect(() => {
-    const filteredPlanets = data.filter((item: Planet) => {
-      return item.name.toLowerCase().indexOf(searchPhrase.toLowerCase()) > -1;
+    const dataCloned = [...data];
+    const filteredByStartWith = dataCloned.filter((planet, index) => {
+      if (planet.name.toLowerCase().startsWith(searchPhrase.toLowerCase())) {
+        dataCloned.splice(index, 1);
+        return true;
+      }
+      return false;
     });
 
-    setFilteredData(filteredPlanets);
+    const filteredBySubstring = dataCloned.filter((item: Planet) => {
+      return item.name.toLowerCase().indexOf(searchPhrase.toLowerCase()) > -1;
+    });
+    setFilteredData([...filteredByStartWith, ...filteredBySubstring]);
   }, [searchPhrase, data]);
 
   return { filteredData };
