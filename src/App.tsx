fad29ce2d51ff5284ec576b321cdbd973aa44d9c
luknp <luk.npl@gmail.com>
2021-11-28 16:57:39 +0100

    style: utility classes and color variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Planets from 'pages/Planets';
+import 'styles/utility_classes.scss';
+import 'styles/index.scss';
 
 function App() {
-  return <Planets />;
+  return (
+    <div className='app-root page-content flex-center'>
+      <Planets />
+    </div>
+  );
 }
 
 export default App;
